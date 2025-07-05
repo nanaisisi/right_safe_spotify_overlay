bddcca6c7a079c88fa99e574ee9d6aaa529ec651
nanaisisi <131863615+nanaisisi@users.noreply.github.com>
2025-07-06 01:40:57 +0900

    formatting

--- public/script.js
+++ public/script.js
@@ -1,15 +1,15 @@
-const trackName = document.getElementById('track-name');
-const artistName = document.getElementById('artist-name');
-const loginLink = document.getElementById('login-link');
+const trackName = document.getElementById("track-name");
+const artistName = document.getElementById("artist-name");
+const loginLink = document.getElementById("login-link");
 
 const ws = new WebSocket(`ws://${location.host}/ws`);
 
 ws.onmessage = (event) => {
-    const data = JSON.parse(event.data);
-    trackName.textContent = data.trackName;
-    artistName.textContent = data.artistName;
+  const data = JSON.parse(event.data);
+  trackName.textContent = data.trackName;
+  artistName.textContent = data.artistName;
 
-    if (loginLink) {
-        loginLink.style.display = 'none';
-    }
+  if (loginLink) {
+    loginLink.style.display = "none";
+  }
 };
